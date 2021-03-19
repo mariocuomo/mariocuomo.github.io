c3f66fded39a307b0682ec802a918cec0100afd3
Mario Cuomo <39244192+mariocuomo@users.noreply.github.com>
2021-03-19 22:02:40 +0100

    Update Chart

--- assets/js/util_mario.js
+++ assets/js/util_mario.js
@@ -15,10 +15,44 @@ new Chart(document.getElementById("doughnut-chart"), {
       title: {
         display: true,
         text: 'My time routine'
-      }
+      },
+      responsive: true,
     }
 });
 
+new Chart(document.getElementById("bar-chart"), {
+  type: 'bar',
+  data: {
+    labels: ["STUDYING", "READING", "SPORT", "HOBBY", "SOCIAL NETWORK"],
+    datasets: [
+      {
+        backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
+        data: [52,15,10,15,8]
+      }
+    ]
+  },
+  options: {
+    title: {
+      display: true,
+      text: 'My time routine'
+    },
+    
+  }
+});
+
+window.onresize = function() { 
+    chartResize();
+
+}; 
+
+function chartResize(){
+  //var rp = document.getElementById("doughnut-chart");
+  if(window.screen.height<600){
+    rp.type='bar';
+    alert(rp.type);
+
+  }
+}
 
 function showBooks() {
   var rp = document.getElementById("Books");
